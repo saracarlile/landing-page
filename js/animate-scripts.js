8b149909f725fc08ca8c1552c403fe1bf4103719
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2018-08-03 10:18:03 -0500

    pause on hover carousel

--- js/animate-scripts.js
+++ js/animate-scripts.js
@@ -1,5 +1,41 @@
-$(document).ready(function () {
-   
+let slideIndex = 1;
+let timer;
+
+
+showSlides = () => {
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+
+$(function () {
+    timer = window.setInterval("showSlides();", 4000);
+
+
+    $('#carousel').mouseenter(function () {
+        ;
+        $('.inner-left').find('h3:nth-of-type(1)').removeClass('animate');
+        $('.inner-left').find('h3:nth-of-type(2)').removeClass('animate-two');
+        $('.add-margin-inner-left').find('p').removeClass('animate-three');
+        $('.inner-left-flex-row').find('button').removeClass('animate-four');
+        $('.inner-right').find('img').removeClass('animate-five');
+        window.clearInterval(timer);
+    });
+
+    $('#carousel').mouseleave(function () {
+        $('.inner-left').find('h3:nth-of-type(1)').addClass('animate');
+        $('.inner-left').find('h3:nth-of-type(2)').addClass('animate-two');
+        $('.add-margin-inner-left').find('p').addClass('animate-three');
+        $('.inner-left-flex-row').find('button').addClass('animate-four');
+        $('.inner-right').find('img').addClass('animate-five');
+        timer = window.setInterval("showSlides();", 4000);
+    });
+
 
     //animation product images on scroll
     $('.fade-in-up').addClass('hidden').waypoint({
@@ -11,8 +47,11 @@ $(document).ready(function () {
 
     //hamgurger nav
 
-    $('.burger-nav').on('click', function(){
+    $('.burger-nav').on('click', function () {
         $('nav ul').toggleClass('open');
     });
 
+
 });
+
+//https://stackoverflow.com/questions/21454318/stop-slideshow-on-mouseover
